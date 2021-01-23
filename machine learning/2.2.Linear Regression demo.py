39ad46aa909cc521d6ffc7744659232bc3ce7f02
zhangcj5131 <zhangcj5@gmail.com>
2021-01-23 12:17:48 +0800

    KNN

---
+++ machine learning/2.2.Linear Regression demo.py
@@ -0,0 +1,20 @@
+from sklearn.linear_model import LinearRegression
+
+x = [[80, 86],
+[82, 80],
+[85, 78],
+[90, 90],
+[86, 82],
+[82, 90],
+[78, 80],
+[92, 94]]
+y = [84.2, 80.6, 80.1, 90, 83.2, 87.6, 79.4, 93.4]
+
+# 实例化API
+estimator = LinearRegression()
+# 使用fit方法进行训练
+estimator.fit(x,y)
+
+print(estimator.coef_)
+
+print(estimator.predict([[100, 80]]))
